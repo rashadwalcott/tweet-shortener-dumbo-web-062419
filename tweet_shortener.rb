16e3b801d0949d7ed897a663fffd0339eb048a1e
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-23 16:17:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ def selective_tweet_shortener(tweets)
 
 def shortened_tweet_truncator(tweets)
     if tweets.length > 140
-      word_substituter(tweets)[0..140] + "..."
+      word_substituter(tweets)[0..137] + "..."
     else
   tweets
 end
