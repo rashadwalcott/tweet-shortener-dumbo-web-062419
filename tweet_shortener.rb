820f3df40c476967018244d56fea5d3ecb20d3d7
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-23 04:17:38 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def dictionary
+   dictionary = {"hello" => "hi","to" => "2", "two" => "2", "too" => "2","for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@","and" => "&"
+}
+end
+
+=begin def word_substituter(tweet_one)
+ 
+  array = tweet_one.split(/ /)
+  array.each do |tweet|
+    dictionary.each do |key,value|
+    if tweet == key
+      tweet = value
+    end
+  end
+  end
+  array.join(' ')
+=end
+
+def word_substituter(tweet_one)
+
+  array = tweet_one.split(/ /)
+  array.map do |tweet|
+    if tweet == dictionary.keys
+      array[tweet] = dictionary.values
+  end
+  end
+  array.join(" ")
+end
