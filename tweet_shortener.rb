22be91078079b2790b852d95030cada872570550
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-23 15:51:12 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,5 @@
 # Write your code here.
+require "pry"
 def dictionary
    hash = {"hello" => "hi","to" => "2", "two" => "2", "too" => "2","for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@","and" => "&"
 }
@@ -6,14 +7,28 @@ end
 
 def word_substituter(tweet_one)
  
-  array = tweet_one.split(/ /)
-  array.each do |tweet|
-    dictionary.each do |word, replace|
-    if tweet.include? word
-      tweet = replace
+ tweet_one.split.map do |tweet|
+   if dictionary.keys.include? (tweet.downcase)
+      tweet = dictionary[tweet.downcase]
+    else
+      tweet
     end
-  end
-  end
-  array.join(' ')
+  end.join(' ')
 end
 
+def bulk_tweet_shortener(tweets)
+  tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet_one)
+ tweet_one.split(" ").map do |tweet|
+   if tweet_one.length > 140
+     word_substituter(tweet)
+   elsif tweet_one <= 140
+     tweet_one
+   end
+ end
+  
+end
